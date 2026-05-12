2938bc016987d4bfad719f7b99ecc349a29d9408
Disan Ayuka Tennakoon <dten781@aucklanduni.ac.nz>
2026-05-12 14:42:43 +1200

    Fixed error in code

--- code/code/game_top_level.vhd
+++ code/code/game_top_level.vhd
@@ -93,7 +93,8 @@ begin
     VGA_VS <= vert_sync_internal;
 
     -- Mirror switches to LEDs for demo
-    LEDR <= SW;
+    LEDR(7 downto 0) <= SW(7 downto 0);
+	LEDR(9) <= SW(9);
 	LEDR(8) <= left_click;
 
     -- Turn off unused seven segment displays
